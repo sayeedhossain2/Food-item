8c863572f754ab9173e2a1a53078cf776c57e2d4
sayeedhossain2 <sayeedh95@gmail.com>
2022-08-29 11:37:30 +0600

    first 4 item added

--- js/food.js
+++ js/food.js
@@ -1,1 +1,34 @@
-console.log("welcome to food");
+const loadMeals = () => {
+  const url = `https://www.themealdb.com/api/json/v1/1/search.php?f=a`;
+
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayMeals(data.meals));
+};
+loadMeals();
+
+const displayMeals = (meals) => {
+  // console.log(meals);
+  const allMeal = document.getElementById("allMeal");
+  meals.forEach((meal) => {
+    console.log(meal);
+    const allmealDiv = document.createElement("div");
+    allmealDiv.classList.add("col");
+    allmealDiv.innerHTML = `
+    
+    <div class="card">
+    <img src="${meal.strMealThumb}" class="card-img-top" alt="..." />
+    <div class="card-body">
+      <h5 class="card-title">${meal.strMeal}</h5>
+      <p class="card-text">
+        This is a longer card with supporting text below as a
+        natural lead-in to additional content. This content is a
+        little bit longer.
+      </p>
+    </div>
+  </div>
+    
+    `;
+    allMeal.appendChild(allmealDiv);
+  });
+};
